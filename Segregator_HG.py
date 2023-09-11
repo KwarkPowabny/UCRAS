7a981a9fe4e9ea37cf03e7e2a73d598132026bc3
Pawel <lubczanskip@gmail.com>
2023-09-11 14:50:19 +0200

    11.09południe

---
+++ Segregator_HG.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 01:14:48 2023
+
+@author: jakubscioch
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 16:40:56 2023
+
+@author: jakubscioch
+"""
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+E=np.load('Hg_E_matrix_g4000.npy')
+spins =  np.load('Hg_S_g800.npy')
+R_min = 600
+R_max = 1800
+NR = 3998
+R = np.linspace(R_min, R_max, NR)
+R_T=np.linspace(R_min, R_max, 3996)
+R_spin=np.linspace(R_min, R_max, 800)
+dr=(R[3997]-R[0])/len(R)
+E_cut = []
+E_cut2 = []
+NR = len(E[0])
+spins_cut=[]
+
+def sort_states_and_interp_spins(E, E_cut, E_cut2, spins_cut):
+
+    for i in range(len(E)-3):
+        if np.abs(np.sum(np.array(E[i])) - NR*E[i][len(E[i])-5]) >1e-5:
+            E_cut.append(E[i])
+            spins_cut.append(spins[i])
+            
+    for i in range(len(E)-3):
+        if np.abs(np.sum(np.array(E[i])) - NR*E[i][len(E[i])-5]) >1e-5:
+            E_cut2.append(E[i])
+                
+
+    E_cut = np.array(E_cut)
+    E_cut2 = np.array(E_cut)
+    spins_cut=np.array(spins_cut)
+    derE2 = np.zeros((len(E_cut) , len(R)))
+    spins_ext = np.zeros((len(E_cut) , len(R)))
+    test_arr = np.zeros((len(E_cut) , len(R)))
+    for i in range(len(spins_cut)):
+        R_arr=np.array(R)
+        R_spin_arr=np.array(R_spin)   
+        spins_cut_arr=np.array(spins_cut[i])
+        spins_interpolate=np.interp(R_arr,R_spin, spins_cut_arr)    
+        spins_ext[i]=spins_interpolate
+
+sort_states_and_interp_spins(E, E_cut, E_cut2, spins_cut)
+
+ik = int(len(480))
+E_sort = []
+for p in range(ik):
+    E_sort.append([])  
+
+for i in range(0,478,1) :
+    plt.plot(R, E_cut[i])    
+    plt.ylim(-30,0)
+    plt.xlim(700,1800)
+plt.show()
+
+
+def Find_state(x,k,j):
+   
+    for i in range(0,480,1)  :
+        q=E_cut2[i][j-1]
+        if q==x:
+        
+            return (i)      
+
+#sortowanie:
+          
+for k in range(0,478,1) :
+    print(k)
+    E_sort[k].append(E_cut[k][0])    
+    E_cut[k][0]=0
+    for j in range(1,3996,1) :  
+ 
+     if len(E_sort[k])==1 or len(E_sort[k])==2:  
+            
+           w=Find_state(E_sort[k][j-1],k,j)
+     
+                                
+           lista3=  []  
+           for t in range(0,480,1):
+            
+            if E_cut[t][j]!=0.0:   
+             q= abs((E_cut[t][j]-E_sort[k][j-1]))
+             lista3.append(q)
+           r=min(lista3)
+           for s in range(0,480,1):
+            if abs((E_cut[s][j]-E_sort[k][j-1])) ==r:
+             E_sort[k].append(E_cut[s][j]) 
+            
+             E_cut[s][j]=0   
+             break 
+                    
+                   
+                             
+     else:
+              w=Find_state(E_sort[k][j-1],k,j)
+        
+                                   
+              lista3=  []  
+              for t in range(0,480,1):
+               
+               if E_cut[t][j]!=0.0:   
+                q= abs((E_cut[t][j]-2*E_sort[k][j-1]+E_sort[k][j-2])/dr)  
+                lista3.append(q)
+              r=min(lista3)
+              for s in range(0,480,1):
+               if abs((E_cut[s][j]-2*E_sort[k][j-1]+E_sort[k][j-2])/dr) ==r:
+                E_sort[k].append(E_cut[s][j]) 
+               
+                E_cut[s][j]=0   
+                break 
+            
+          
+                     
+                               
+                            
+for i in range(250,310,1) :
+    for j in range(len(R)-3):
+        dE2=(E_sort[i][j+1]-E_sort[i][j])/(dr)
+        derE2[i][j] = dE2  
+
+
+
+
+
+
+
+with open("Hg_E_matrix_g4000_sort", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(E_sort)
+
+ 
+for i in range(260,308,1):
+
+    f, (ax1, ax2,ax3) = plt.subplots(3, 1, sharex=True)
+    ax1.set_title('State'+str(i))
+    ax1.set_ylabel('E(GHz)')
+    ax2.set_ylabel('GHz/Bohr')
+    ax3.set_ylabel('Spin')
+    plt.xlabel('R(Bohr)')
+    
+    ax1.scatter(R_T, E_sort[i-1],s=1,color='red',alpha=0.3)
+    ax1.scatter(R_T, E_sort[i],s=6,color='blue',alpha=0.3)
+    ax1.scatter(R_T, E_sort[i+1],s=3,color='green',alpha=0.3)
+  #  ax1.scatter(R_T, E_sort[i-2],s=1,color='black',alpha=0.5)
+   # ax1.scatter(R_T, E_sort[i+2],s=1,color='purple',alpha=0.3)
+   # ax1.set_ylim(-23.3,-23.27)
+    
+    ax2.scatter(R, derE2[i-1],s=4,color='red',alpha=0.3)
+    ax2.scatter(R, derE2[i],s=4,color='blue',alpha=0.4)
+    ax2.scatter(R,derE2[i+1],s=4,color='green',alpha=0.3)
+   # ax2.scatter(R, derE2[i+2],s=1,color='black',alpha=0.4)
+    #ax2.scatter(R,derE2[i-2],s=1,color='purple',alpha=0.3)
+   # ax2.set_xlim(808.5,810)
+    #ax2.set_ylim(-0.015,0.015)
+    
+    ax3.scatter(R, spins_ext[i-1],s=2,color='red',alpha=0.4)
+    ax3.scatter(R, spins_ext[i],s=2,color='blue',alpha=0.4)
+    ax3.scatter(R, spins_ext[i+1],s=2,color='green',alpha=0.4)
+   # ax3.set_ylim(0.30,0.80)
+   
+    
+    plt.show()
+   # f.savefig('New_Sort_HG_plot_' + str(i) + '.pdf')
+    
+for i in range(0,478,1) :
+ plt.plot(R_T,E_sort[i])    
+ plt.ylim(-30,0)
+ plt.xlim(700,1800)
+plt.show()
+
+#for i in range(250,310,1):
+  
+# f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+# ax1.set_title('State'+str(i))  
+ #ax1.scatter(R,E_cut[i],s=2,color='red',alpha=0.4)
+ #ax2.scatter(R_T,E_sort[i],s=2,color='red',alpha=0.4)
+ #ax2.set_ylim(-24,-22.9)
+# ax1.set_ylim(-24,-22.9)
+
+# plt.show()
--- test.py
+++ test.py
@@ -1,7 +1,57 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-E = np.zeros((2,3))
-for i in range(2):
-    for j in range(3):
-        E[i][j] = i**2*j
-        print(1**(-j))
+ik = 480
+E_sort = []
+for p in range(ik):
+    E_sort.append([])
+R_min = 600
+R_max = 1800
+NR = 3998
+#R = np.linspace(R_min, R_max, NR)
+R_T=np.linspace(R_min, R_max, NR - 2)
+Y=pd.read_csv("Hg_E_matrix_g4000_sort")
+E_sort = np.array(Y)
+E=np.load('Hg_E_matrix_g4000.npy')
+spins =  np.load('Hg_S_g800.npy')
+R = np.linspace(R_min, R_max, NR)
+R_T=np.linspace(R_min, R_max, 3996)
+R_spin=np.linspace(R_min, R_max, 800)
+dr=(R[3997]-R[0])/len(R)
+E_cut = []
+E_cut2 = []
+spins_cut=[]
+
+xlim = (700,1800)#(656, 658)#
+ylim = (-30,0)#(-11.4, -11.33)#
+for i in range(0,477,1) :
+ plt.plot(R_T,E_sort[i])    
+ plt.ylim(ylim[0], ylim[1])
+ plt.xlim(xlim[0], xlim[1])
+plt.savefig("Hg_states_after_sort.pdf")
+plt.show()
+
+for i in range(len(E)-3):
+        if np.abs(np.sum(np.array(E[i])) - NR*E[i][len(E[i])-5]) >1e-5:
+            E_cut.append(E[i])
+            spins_cut.append(spins[i])                
+
+E_cut = np.array(E_cut)
+spins_cut=np.array(spins_cut)
+derE2 = np.zeros((len(E_cut) , len(R)))
+spins_ext = np.zeros((len(E_cut) , len(R)))
+test_arr = np.zeros((len(E_cut) , len(R)))
+for i in range(len(spins_cut)):
+    R_arr=np.array(R)
+    R_spin_arr=np.array(R_spin)   
+    spins_cut_arr=np.array(spins_cut[i])
+    spins_interpolate=np.interp(R_arr,R_spin, spins_cut_arr)    
+    spins_ext[i]=spins_interpolate
+
+for i in range(0,478,1) :
+    plt.plot(R, E_cut[i])    
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlim(xlim[0], xlim[1])
+plt.savefig("Hg_states_before_sort.pdf")
+plt.show()
